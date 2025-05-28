dd7d3948e199795f510916c6302a7520f5fab36c
shalom-gnassounou <shalom.gnassounou@labom2iformation.fr>
2025-05-28 11:53:58 +0200

    frise chronologique et articles

---
+++ asset/js/script.js
@@ -0,0 +1,11 @@
+const timeLineDots = document.querySelectorAll(".timeline-dots");
+const carousel = document.querySelector("#kpopCarousel");
+const bsCarousel =
+  bootstrap.Carousel.getInstance(carousel) || new bootstrap.Carousel(carousel);
+
+for (const timeLineDot of timeLineDots) {
+  timeLineDot.addEventListener("click", () => {
+    const slideIndex = parseInt(timeLineDot.getAttribute("data-slide"));
+    bsCarousel.to(slideIndex);
+  });
+}
